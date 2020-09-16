a2a7504436e0350e7dbf23c62949371ac5fa842b
valeriasheredia <valeheredia97@gmail.com>
2020-09-16 15:17:28 -0300

    Add files via upload

---
+++ ADMINISTRACION/DATOS/TipoFormulario.vb
@@ -0,0 +1,46 @@
+﻿Public Enum TipoFormulario
+    AltaDeposito
+    ModificarDeposito
+    EliminarDeposito
+    BuscarDeposito
+
+    AltaProducto
+    ModificarProducto
+    EliminarProducto
+    BuscarProducto
+
+    AltaProveedor
+    ModificarProveedor
+    EliminarProveedor
+    BuscarProveedor
+
+    AltaTipoProducto
+    ModificarTipoProducto
+    EliminarTipoProducto
+    BuscarTipoProducto
+
+    AltaCliente
+    ModificarCliente
+    EliminarCliente
+    BuscarCliente
+
+    AltaUsuarios
+    ModificarUsuarios
+    EliminarUsuarios
+    BuscarUsuarios
+
+    AltaFormaPago
+    ModificarFormaPago
+    EliminarFormaPago
+    BuscarFormaPago
+
+    AltaServicio
+    ModificarServicio
+    EliminarServicio
+    BuscarServicio
+
+    AltaVenta
+    ModificarVenta
+    EliminarVenta
+    BuscarVenta
+End Enum
