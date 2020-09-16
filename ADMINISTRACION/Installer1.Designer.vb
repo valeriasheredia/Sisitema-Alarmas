8fcbed1e3fc0ace4d3fb6f59e19a671d125b8a79
valeriasheredia <valeheredia97@gmail.com>
2020-09-16 15:13:28 -0300

    Add files via upload

---
+++ ADMINISTRACION/Installer1.Designer.vb
@@ -0,0 +1,47 @@
+﻿<System.ComponentModel.RunInstaller(True)> Partial Class Installer1
+    Inherits System.Configuration.Install.Installer
+
+    'Installer reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de componentes
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de componentes requiere el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de componentes.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(0, 0)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(0, 0)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(120, 20)
+        Me.NumericUpDown1.TabIndex = 0
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+
+End Class
