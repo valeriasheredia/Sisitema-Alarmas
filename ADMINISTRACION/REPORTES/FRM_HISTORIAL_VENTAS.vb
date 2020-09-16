5c6bdcfb4653c274cac80ade7ee46c81bd8f8726
valeriasheredia <valeheredia97@gmail.com>
2020-09-16 15:22:58 -0300

    Add files via upload

---
+++ ADMINISTRACION/REPORTES/FRM_HISTORIAL_VENTAS.vb
@@ -0,0 +1,9 @@
+﻿Public Class FRM_HISTORIAL_VENTAS
+    Public id_venta As Integer
+
+    Private Sub FRM_HISTORIAL_VENTAS_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Dim Historial_Ventas As New Reporte_Historial_Ventas
+        Historial_Ventas.SetParameterValue("@id_venta", id_venta)
+        CrystalReportViewer1.ReportSource = Historial_Ventas
+    End Sub
+End Class
---
+++ ADMINISTRACION/REPORTES/FRM_REPORTE_VENTA.vb
@@ -0,0 +1,9 @@
+﻿Public Class FRM_REPORTE_VENTA
+    Public id_venta As Integer
+
+    Private Sub FRM_REPORTE_VENTA_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Dim ReporteVenta As New Reporte_Venta
+        ReporteVenta.SetParameterValue("@id_venta", id_venta)
+        CrystalReportViewer1.ReportSource = ReporteVenta
+    End Sub
+End Class
